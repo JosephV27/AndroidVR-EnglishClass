398b67a0fc98ca4de2b304f5ed8b4928349dc9eb
squesadav <sebas.quesada790@gmail.com>
2022-04-03 22:09:32 -0600

    added on camera sight actions

---
+++ English Class/Assets/Scripts/Controllers/StudentController.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentController : MonoBehaviour
+{
+    /// <summary>
+    /// The material to use when this object is inactive (not being gazed at).
+    /// </summary>
+    public Material InactiveMaterial;
+
+    /// <summary>
+    /// The material to use when this object is active (gazed at).
+    /// </summary>
+    public Material GazedAtMaterial;
+
+    // The objects are about 1 meter in radius, so the min/max target distance are
+    // set so that the objects are always within the room (which is about 5 meters
+    // across).
+    private const float _minObjectDistance = 2.5f;
+    private const float _maxObjectDistance = 3.5f;
+    private const float _minObjectHeight = 0.5f;
+    private const float _maxObjectHeight = 3.5f;
+
+    private Renderer _myRenderer;
+    private Vector3 _startingPosition;
+
+    private Animator animator;
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    public void Start()
+    {
+        //_startingPosition = transform.parent.localPosition;
+        _myRenderer = GetComponent<Renderer>();
+        SetMaterial(false);
+
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Teleports this instance randomly when triggered by a pointer click.
+    /// </summary>
+    public void TeleportRandomly()
+    {
+        // Picks a random sibling, activates it and deactivates itself.
+        int sibIdx = transform.GetSiblingIndex();
+        int numSibs = transform.parent.childCount;
+        sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+        GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+
+        // Computes new object's location.
+        float angle = Random.Range(-Mathf.PI, Mathf.PI);
+        float distance = Random.Range(_minObjectDistance, _maxObjectDistance);
+        float height = Random.Range(_minObjectHeight, _maxObjectHeight);
+        Vector3 newPos = new Vector3(Mathf.Cos(angle) * distance, height,
+                                     Mathf.Sin(angle) * distance);
+
+        // Moves the parent to the new position (siblings relative distance from their parent is 0).
+        transform.parent.localPosition = newPos;
+
+        randomSib.SetActive(true);
+        gameObject.SetActive(false);
+        SetMaterial(false);
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it starts gazing at this GameObject.
+    /// </summary>
+    public void OnPointerStudentEnter()
+    {
+        SetMaterial(true);
+        animator.CrossFade("dancing", 0.25f);
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it stops gazing at this GameObject.
+    /// </summary>
+    public void OnPointerExit()
+    {
+        SetMaterial(false);
+        animator.CrossFade("idle", 0.25f);
+        audioSource.Stop();
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        TeleportRandomly();
+    }
+
+    /// <summary>
+    /// Sets this instance's material according to gazedAt status.
+    /// </summary>
+    ///
+    /// <param name="gazedAt">
+    /// Value `true` if this object is being gazed at, `false` otherwise.
+    /// </param>
+    private void SetMaterial(bool gazedAt)
+    {
+        if (InactiveMaterial != null && GazedAtMaterial != null)
+        {
+            _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
+        }
+    }
+}
